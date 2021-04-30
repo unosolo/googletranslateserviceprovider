7892eddc110349ecbd8f198b4736900f1279bd80
unosolo <jchbarreto@gmail.com>
2021-04-30 19:20:25 -0400

    Refactoring the signature of the translate method of the interface

--- app/Interfaces/TranslatorInterface.php
+++ app/Interfaces/TranslatorInterface.php
@@ -4,5 +4,5 @@ namespace App\Interfaces;
 
 interface TranslatorInterface
 {
-  public function translate(string $text, string $source_language, string $target_language);
+  public function translate(string $from_text, string $source_language, string $target_language) :string;
 }
