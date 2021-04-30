d2b9f8b5c14b4d11badb8c44699cf5f3c30fa67d
unosolo <jchbarreto@gmail.com>
2021-04-30 19:22:23 -0400

    Adding new Google Translator Service

--- app/Http/Controllers/TranslatePage.php
+++ app/Http/Controllers/TranslatePage.php
@@ -2,17 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Interfaces\TranslatorInterface;
+use App\Services\GoogleTranslatorService;
 use Illuminate\Http\Request;
 
 class TranslatePage extends Controller
 {
-  public function __invoke(Request $request)
+  public function __invoke(Request $request, GoogleTranslatorService $translator)
   {
     $text = $request->query('t', '');
     $source_language = $request->query('src', '');
     $target_language = $request->query('trg', '');
     return view('pages.translate', [
-      'text' => $text,
+      'from_text' => $text,
+      'into_text' => $translator->translate($text, $source_language, $target_language),
       'source_language' => $source_language,
       'target_language' => $target_language,
     ]);
